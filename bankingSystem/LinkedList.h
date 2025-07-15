e0a561f20ab20001af7a4394f59ab0559e571203
mehdi0-0s <mehdii84sl@gmail.com>
2025-07-15 15:41:01 +0330

    Implement constructor and destructor for linkedList

--- bankingSystem/LinkedList.h
+++ bankingSystem/LinkedList.h
@@ -1,9 +1,32 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
+#include "Node.h"
 
+template<typename T>
 class LinkedList{
+private:
+    Node<T> *head;
 
+public:
+    LinkedList()
+    {
+        head = nullptr;
+    }
+
+    ~LinkedList()
+    {
+        Node<T> *current = head;
+        Node<T> *nextNode = nullptr;
+        while (current != nullptr) {
+            nextNode = current->next;
+            delete current;
+            current = nextNode;
+        }
+        head = nullptr;
+    }
+
+    Node<T>* getHead(){return head;}
 };
 
 #endif // LINKEDLIST_H
