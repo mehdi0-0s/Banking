895f4250935a569c0ff78e62d186ffa13de4f63c
mehdi0-0s <mehdii84sl@gmail.com>
2025-07-15 18:24:40 +0330

    (user): add constructure and setters and getters declarations  in user.h

--- bankingSystem/user.h
+++ bankingSystem/user.h
@@ -1,10 +1,34 @@
 #ifndef USER_H
 #define USER_H
 
+#include <QString>
+
 class User
 {
+protected:
+    QString name;
+    QString lastName;
+    QString nationalCode;
+    QString username;
+    QString password;
+    int age;
+
 public:
-    User();
+    User(QString name, QString lastName, QString nationalCode, QString username, QString password, int age);
+
+    QString getName() ;
+    QString getLastName() ;
+    QString getNationalCode() ;
+    QString getUsername() ;
+    QString getPassword() ;
+    int getAge();
+
+    void setName(QString newName);
+    void setLastName(QString newLastName);
+    void setNationalCode();
+    void setUsername();
+    void setAge(int newAge);
+    void setPassword(QString newPassword);
 };
 
 #endif // USER_H
