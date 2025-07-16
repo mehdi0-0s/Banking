eea208bd447bfad6663eb2d4a2644096c170baa6
mehdi0-0s <mehdii84sl@gmail.com>
2025-07-16 17:06:36 +0330

    (savingAccount) : add constructure and calculateInterest method

--- bankingSystem/savingaccount.cpp
+++ bankingSystem/savingaccount.cpp
@@ -1,3 +1,11 @@
 #include "savingaccount.h"
 
-SavingAccount::SavingAccount() {}
+SavingAccount::SavingAccount(QString cardNumber,QString accountNumber,QString shebaNumber,
+                             QString cvv2, double balance,QDate expDate,QString pin,QString pin2)
+                :Account(cardNumber, accountNumber, shebaNumber, cvv2, balance, expDate, pin, pin2)
+{}
+
+double SavingAccount::calculateInterest(double interestRate)
+{
+    return this->balance * interestRate;
+}
--- bankingSystem/savingaccount.h
+++ bankingSystem/savingaccount.h
@@ -1,10 +1,15 @@
 #ifndef SAVINGACCOUNT_H
 #define SAVINGACCOUNT_H
 
-class SavingAccount
+#include "account.h"
+
+class SavingAccount :public Account
 {
 public:
-    SavingAccount();
+    SavingAccount(QString cardNumber,QString accountNumber,QString shebaNumber,
+                   QString cvv2, double balance,QDate expDate,QString pin,QString pin2);
+
+    double calculateInterest(double interestRate = 0.1);
 };
 
 #endif // SAVINGACCOUNT_H
