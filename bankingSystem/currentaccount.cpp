295da7ce573af60aeaffa7dd3e0fe2622bcf070e
mehdi0-0s <mehdii84sl@gmail.com>
2025-07-16 17:30:01 +0330

    (currentAccount) : add constructure and generateMiniStatement method

--- bankingSystem/currentaccount.cpp
+++ bankingSystem/currentaccount.cpp
@@ -1,3 +1,11 @@
 #include "currentaccount.h"
 
-CurrentAccount::CurrentAccount() {}
+CurrentAccount::CurrentAccount(QString cardNumber,QString accountNumber,QString shebaNumber,
+                               QString cvv2, double balance,QDate expDate,QString pin,QString pin2)
+                :Account(cardNumber, accountNumber, shebaNumber, cvv2, balance, expDate, pin, pin2)
+{}
+
+void CurrentAccount::generateMiniStatement()
+{
+
+}
--- bankingSystem/currentaccount.h
+++ bankingSystem/currentaccount.h
@@ -1,10 +1,16 @@
 #ifndef CURRENTACCOUNT_H
 #define CURRENTACCOUNT_H
 
-class CurrentAccount
+#include "account.h"
+
+
+class CurrentAccount :public Account
 {
 public:
-    CurrentAccount();
+    CurrentAccount(QString cardNumber,QString accountNumber,QString shebaNumber,
+                    QString cvv2, double balance,QDate expDate,QString pin,QString pin2);
+
+     void generateMiniStatement();
 };
 
 #endif // CURRENTACCOUNT_H
