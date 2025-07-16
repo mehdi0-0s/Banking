bad59a02aa01e37cf1d113d6a06ae43400336dac
mehdi0-0s <mehdii84sl@gmail.com>
2025-07-16 18:21:18 +0330

    (checkingAccount) : add constructure and grantLoan method

--- bankingSystem/checkingaccount.h
+++ bankingSystem/checkingaccount.h
@@ -1,10 +1,16 @@
 #ifndef CHECKINGACCOUNT_H
 #define CHECKINGACCOUNT_H
 
-class CheckingAccount
+#include "account.h"
+
+class CheckingAccount : public Account
 {
 public:
-    CheckingAccount();
+    CheckingAccount(QString cardNumber,QString accountNumber,QString shebaNumber,
+                    QString cvv2, double balance,QDate expDate,QString pin,QString pin2);
+
+    bool grantLoan(double amount);
+
 };
 
 #endif // CHECKINGACCOUNT_H
